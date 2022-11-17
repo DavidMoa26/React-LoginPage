59812d9a7c30b8806c196d10efa51f6eb87b9217
David <dumo0533dm@gmail.com>
2022-11-17 15:14:16 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import Authentication from "./routes/authentication/authentication.component";
 
 
 
-const Shop = () => {
-  return <h1>I'm Shop Page</h1>;
-};
-
 const App = () => {
   return (
     <Authentication />
