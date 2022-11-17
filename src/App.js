64214b74236da305dee4369c03b4989b07961c21
David <dumo0533dm@gmail.com>
2022-11-17 14:20:24 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 
 
@@ -11,13 +8,7 @@ const Shop = () => {
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-      </Route>
-    </Routes>
+    <Authentication />
   );
 };
 
